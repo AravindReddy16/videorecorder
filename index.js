4c1d9f28c27b5b0c9b55a24d2cee2c2c88449230
Aravind160324 <aravindreddygudi16@gmail.com>
2024-06-22 08:46:04 +0530

    first

---
+++ index.js
@@ -0,0 +1,40 @@
+let videocam = document.querySelector('#videocam')
+let vstart = document.querySelector('#v-start')
+let vrotate = document.querySelector('#v-rotate')
+let vstop = document.querySelector('#v-stop')
+
+let front = true
+
+const rotate = () => {
+    front = !front
+    return front
+}
+
+navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+        facingMode: "environment"
+    },
+}).then((stream) => {
+    videocam.autoplay = true
+    videocam.srcObject = stream
+    let recorder = new MediaRecorder(stream,{
+        mimeType: "video/webm"
+    })
+
+    vstart.addEventListener('click', () => {
+        recorder.start(1000)
+        vstop.disabled = false
+        vstart.disabled = true
+    })
+
+    vstop.addEventListener('click', ()=>{
+        recorder.stop()
+        vstop.disabled = true
+        vstart.disabled = false
+    })
+
+    vrotate.addEventListener('click', ()=>{
+        rotate()
+    })
+})
